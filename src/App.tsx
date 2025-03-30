6ec3cd51ef7c083e78b40bc246c08f946f13706b
Thiara Fernandes <108100150+ThiaraFernandes@users.noreply.github.com>
2025-03-30 10:40:40 -0300

    Primeiro Commit - Projeto conversor de moedas

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from "./pages/Home"
+import { GlobalStyle, Container } from './pages/styles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Container>
+        <Home />
+      </Container>
+    </>
+  );
+}
+
+export default App
